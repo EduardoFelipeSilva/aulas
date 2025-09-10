6cddad7644370dc65ca7f741ac5b77d3e5f34599
EduardoFelipeSilva <eduardo.silva338@fatec.sp.gov.br>
2025-09-10 09:56:55 -0300

    Reogarnização

--- 2 Semestre/Aula 2/Aula2/src/Motorista.java
+++
@@ -1,5 +0,0 @@
-public class Motorista {
-    private String nome, telefone, sexo, cpf, categoria;
-    private int cnh;
-
-}
--- 2 Semestre/sexta aula 3 vetor/src/vetorTeste.java
+++
@@ -1,9 +0,0 @@
-public class vetorTeste {
-    int[] teste = new int[5];
-
-    teste[0] = 10;
-    teste[1] = 20;
-    teste[2] = 30;
-    teste[3] = 40;
-    teste[4] = 50;
-}
